d99e2151d4b83fa7aa6acb3e4e10f4ce71211d8f
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2018-01-19 13:16:54 +0300

    solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function connectFour(board) {
-    // your solution here
+  const arr = board;
+
+  const R = 'R';
+  const Y = 'Y';
+  const U = '-';
+  const [ x, y ] = [ arr[0].length, arr.length ];
+
+  const isFilled = d => d === R || d === Y;
+
+  const hasNeighbord = (i, j) => (i > -1 && i < x) && (j > -1 && j < y)
+
+  const checkLeft = (i, j) => {
+    return hasNeighbord(i, j - 1) ? arr[i][j - 1] === arr[i][j] : false;
+  }
+
+  const checkUp = (i, j) => {
+    return hasNeighbord(i - 1, j) ? arr[i - 1][j] === arr[i][j] : false;
+  }
+
+  const checkUpperLeft = (i, j) => {
+    return hasNeighbord(i - 1, j - 1) ? arr[i - 1][j - 1] === arr[i][j] : false;
+  }
+
+  for ( let i = 0; i < y; i++) {
+      for ( let j = 0; j < x; j++) {
+        if (isFilled(arr[i][j])) {
+          if (
+            (checkLeft(i, j) && checkLeft(i, j - 1) && checkLeft(i, j - 2)) ||
+            (checkUp(i, j) && checkUp(i - 1, j) && checkUp(i - 2, j)) ||
+            (checkUpperLeft(i, j) && checkUpperLeft(i - 1, j - 1) && checkUpperLeft(i - 2, j - 2))
+          ) {
+            return arr[i][j];
+          }
+        }
+      }
+  }
+  return 'That is not the end!';
 }
